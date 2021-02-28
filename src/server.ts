9f0f4a74599b76ac43933bdf07c84ec826b01135
phdev <phdev182@gmail.com>
2021-02-28 15:54:11 -0400

    Eslint & Jest

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 import '@controllers/UserController';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'});
-}); 
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333);
---
+++ src/tests/FirstTest.spec.ts
@@ -0,0 +1,7 @@
+import { User } from '../models/User'
+
+test('it should be ok', () => {
+  const user = new User()
+  user.name = 'Pedro'
+  expect(user.name).toEqual('Pedro')
+})
